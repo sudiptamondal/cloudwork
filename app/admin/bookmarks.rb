26cb7824ffd3eef4f569a721220ddccf33694c66
Sudipta Mondal <sudiptamondal05@gmail.com>
2021-09-05 14:42:41 +0530

    Added Bookmark model

---
+++ app/admin/bookmarks.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Bookmark do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :text, :url
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:text, :url]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
