d8376dcd82ee717f6542ee8ad68b98016718dcc6
Sudipta Mondal <sudiptamondal05@gmail.com>
2021-10-25 00:52:22 +0530

    Added admin home

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @pi_temp = Sensor.where(device: "pi").order(created_at: :desc).limit(1).first.temperature
   end
 end
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,30 @@
 module DashboardHelper
+	def pi_temp
+		if @pi_temp.to_i > 60
+			temp_class="bg-danger"
+			font_color="white"
+		elsif @pi_temp.to_i > 50 and @pi_temp.to_i <= 60
+			temp_class="bg-warning"
+			font_color="white"
+		elsif @pi_temp.to_i < 50
+			temp_class="bg-light"
+			font_color="black"
+		else
+			temp_class="bg-secondary"
+			font_color="white"
+		end
+			
+			
+		content_tag(
+				:div,
+				content_tag(
+					:div,
+					"Rasperry Pi running @ #{@pi_temp}",
+					class: "card-body",
+					style: "color: #{font_color}"
+				),
+				class: "card #{temp_class} text-white shadow"
+			)
+			      
+	end
 end
